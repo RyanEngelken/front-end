631d64486b478761ab7ac8ff37d0095276737b4f
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-27 13:08:44 -0400

    completed front end and tested functionality

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,9 @@ addEventListener("DOMContentLoaded", async function() {
 
     let html = "";
     for (let song of songs){
-        html += `<li>${song.title} - ${song.artist}</li>`
+        let songID = song._id; // get the ID of the song
+        html += `<li>${song.title} - ${song.artist} - <a href="details.html?id=${songID}">Details</a> - <a href="edit.html?id=${songID}">Edit Song</a> </li>`
     }
 
-    this.document.querySelector("#addedsong").innerHTML = html;
+    this.document.querySelector("#list_of_songs").innerHTML = html;
 })
