b4c1c8001e5ddf0a922e9aefeaa84897419f13a2
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-04-30 14:16:43 -0400

    fixing pathing

--- scripts/auth.js
+++ scripts/auth.js
@@ -5,24 +5,20 @@ class Auth{
         this.validateAuth(auth)
     }
 
-    validateAuth(auth){
-        if(auth != 1){
-            window.location.replace("https://ryanengelken.github.io/front-end/login.html");
-
-        }
-        else {
-            document.querySelector("body").style.display = "block";
-
-        }
+validateAuth(auth){
+    if(auth != 1){
+        window.location.replace("/front-end/login.html");
     }
-
-    logOut(){
-        localStorage.removeItem("auth");
-        localStorage.removeItem("token");
-        localStorage.removeItem("uname");
-
-       window.location.replace("https://ryanengelken.github.io/front-end/login.html");
-
+    else {
+        document.querySelector("body").style.display = "block";
     }
+}
+
+logOut(){
+    localStorage.removeItem("auth");
+    localStorage.removeItem("token");
+    localStorage.removeItem("uname");
+    window.location.replace("/front-end/login.html");
+}
     
 }
